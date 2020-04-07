6a11f600e2ed36d17b4c30e14cfa247e14e9a33c
nidhirajkotia <nidhi.b999@gmail.com>
2020-04-07 18:41:57 +0530

    Update test.js

--- mocha/test.js
+++ mocha/test.js
@@ -1,6 +1,39 @@
 const assert = require('assert');
 const fs = require('fs');
 
+// mocha
+
+let employee = require('../controller/employee');
+
+//Require the dev-dependencies
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../index');
+let should = chai.should();
+chai.use(chaiHttp);
+
+describe('employee', () => {
+  /*
+  * Test the /GET route
+  */
+  describe('/GET employee', () => {
+      it('it should GET all the employee', (done) => {
+        chai.request(server)
+            .get('/employeelist')
+            .end((err, res) => {
+                  res.should.have.status(200);
+                  res.body.should.be.a('array');
+                  res.body.length.should.be.eql(0);
+              done();
+            });
+      });
+  });
+
+});
+// end mocha
+
+
+
 function iThrowError(msg) {
     throw new Error(msg);
 }
