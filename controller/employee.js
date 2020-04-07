f6378692ad6da0b985e60b1736c8d3b75bd5de10
nidhirajkotia <nidhi.b999@gmail.com>
2020-04-07 22:40:57 +0530

    Update employee.js

--- controller/employee.js
+++ controller/employee.js
@@ -1,14 +1,22 @@
 const db = require("../model/dboperation_postgres");
 
 module.exports = {
-    // Method to fetch all the employess from db
-    employee_list: (req, res) => {
-      db.list(req,res);
-    },    
-
+      
     // Method to add employee in db
-    add_employee: (req, res) => {
+    create_employee: (req, res) => {
          db.add(req,res);
+    },
+
+    // Method to fetch all the employess from db
+    view_employee: (req, res) => {
+      console.log('view_employee');
+      db.list(req,res);
+    }, 
+
+    // Method to delete employee by ID in db
+    delete_employee: (req, res) => {
+      console.log('delete_employee');
+         db.delete(req,res);
     }
     
 };
