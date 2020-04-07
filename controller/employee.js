f5d4f75515478e70a28484b35ffafe5f9e0237e2
nidhirajkotia <nidhi.b999@gmail.com>
2020-04-07 10:35:58 +0530

    Update employee.js

--- controller/employee.js
+++ controller/employee.js
@@ -2,12 +2,12 @@ const db = require("../model/dboperation_postgres");
 
 module.exports = {
     // Method to fetch all the employess from db
-    employeelist: (req, res) => {
+    employee_list: (req, res) => {
       db.list(req,res);
     },    
 
     // Method to add employee in db
-    addemployee: (req, res) => {
+    add_employee: (req, res) => {
          db.add(req,res);
     }
     
