81694dfd6bdf715660086031852fb0e4721b04a0
nidhirajkotia <nidhi.b999@gmail.com>
2020-04-13 14:33:11 +0530

    Update employee.js

--- controller/employee.js
+++ controller/employee.js
@@ -9,13 +9,36 @@ const options = {
     resetTimeout: 10000 // After 10 seconds, try again.
 };
 
-// Implement circuit breaker on view_employee method
-var circuit = new circuitBreaker(db.list, options);
+// Register circuit breaker events
+function call_func(dynamicobj) {
+    var circuit = new circuitBreaker(dynamicobj, options);
+    circuit.on('open', (result) => {
+        console.log("open circuit...");
+    });
 
+    circuit.on('success', (result) => {
+        console.log("success circuit...")
+    });
+
+    circuit.on('timeout', (result) => {
+        console.log("timeout circuit...");
+    });
+
+    circuit.on('halfOpen', (result) => {
+        console.log("halfOpen circuit...");
+    });
+
+    circuit.on('close', (result) => {
+        console.log("close circuit...")
+    });
+    return circuit;
+
+}
 module.exports = {
     // Method to add employee in db
     create_employee: (req, res) => {
-        db.add(req, res).then(function(resp) {
+        var breaker = call_func(db.add);
+        breaker.fire(req, res).then(function(resp) {
                 res.status(200).send("Employee added successfully")
             })
             .catch(function(err) {
@@ -25,8 +48,8 @@ module.exports = {
 
     // Method to fetch all the employess from db
     view_employee: (req, res) => {
-
-        circuit.fire(req, res).then(function(resp) {
+        var breaker = call_func(db.list);
+        breaker.fire(req, res).then(function(resp) {
                 res.status(200).send(resp);
             })
             .catch(function(err) {
@@ -36,8 +59,8 @@ module.exports = {
 
     // Method to delete employee by ID in db
     delete_employee: (req, res) => {
-
-        db.delete(req, res).then(function(resp) {
+        var breaker = call_func(db.delete);
+        breaker.fire(req, res).then(function(resp) {
                 res.status(200).send("Employee deleted successfully");
             })
             .catch(function(err) {
@@ -47,23 +70,3 @@ module.exports = {
     }
 
 };
-
-circuit.on('open', (result) => {
-    console.log("open circuit...");
-});
-
-circuit.on('success', (result) => {
-    console.log("success circuit...")
-});
-
-circuit.on('timeout', (result) => {
-    console.log("timeout circuit...");
-});
-
-circuit.on('halfOpen', (result) => {
-    console.log("halfOpen circuit...");
-});
-
-circuit.on('close', (result) => {
-    console.log("close circuit...")
-});
