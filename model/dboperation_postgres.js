513f8ea3663e9195677faafd1101b6e938da5d01
nidhirajkotia <nidhi.b999@gmail.com>
2020-04-13 13:07:40 +0530

    Update dboperation_postgres.js

--- model/dboperation_postgres.js
+++ model/dboperation_postgres.js
@@ -1,6 +1,7 @@
 // Connect with postgres database
 const pg = require('pg');
 const Pool = require('pg').Pool;
+var Promise = require('promise');
 
 pg.defaults.ssl = true;
 
@@ -23,28 +24,34 @@ module.exports = {
             emp_email,
             emp_gender
         } = req.body;
-
-        pool
-            .query('INSERT INTO employee (firstname, lastname, email, gender) VALUES ($1, $2, $3, $4)', [emp_firstname, emp_lastname, emp_email, emp_gender])
-            .then(resp => res.status(200).send(`Employee added successfully`))
-            .catch(err => {
-                console.log(err);
-                res.status(408).send("Error while adding employee");
-            })
+        return new Promise((resolve, reject) => {
+            pool
+                .query('INSERT INTO employee (firstname, lastname, email, gender) VALUES ($1, $2, $3, $4)', [emp_firstname, emp_lastname, emp_email, emp_gender])
+                .then(resp => {
+                    resolve(resp);
+                })
+                .catch(err => {
+                    console.log(err);
+                    reject(err);
+                })
+        });
     },
 
     // Method to fetch all the employess using promise from db
     list: (req, res) => {
         // Run a single query on the database
+        return new Promise((resolve, reject) => {
+            pool
+                .query('select * from employee;')
+                .then(resp => {
+                    resolve(resp.rows);
+                })
+                .catch(err => {
+                        reject(err);
+                    }
 
-        pool
-            .query('select * from employee;')
-            .then(resp => res.status(200).send(resp.rows))
-            .catch(err => {
-                    console.log(err);
-                    res.status(408).send("Error while fetching employee");
-                }
-            )
+                )
+        });
     },
 
     // Method to delete employee using promise in db
@@ -53,12 +60,15 @@ module.exports = {
             emp_id
         } = req.body;
 
-        pool
-            .query('DELETE FROM employee WHERE id = $1', [emp_id])
-            .then(resp => res.status(200).send(`Employee deleted successfully`))
-            .catch(err => {
-                console.log(err);
-                res.status(408).send("Error while deleting employee");
-            })
+        return new Promise((resolve, reject) => {
+            pool
+                .query('DELETE FROM employee WHERE id = $1', [emp_id])
+                .then(resp => {
+                    resolve(resp);
+                })
+                .catch(err => {
+                    reject(err);
+                })
+        });
     }
 };
